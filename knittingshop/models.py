68e228d79e307e83aece37df73efd633326bd306
Black Bart <brightcolors46@gmail.com>
2019-06-17 01:16:57 +0300

    Added upload picture to Item

--- knittingshop/models.py
+++ knittingshop/models.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils import timezone
 
 
@@ -8,10 +9,24 @@ class Item(models.Model):
     item_name = models.CharField(max_length=50)
     item_description = models.TextField()
     item_main_photo_path = models.CharField(max_length=120, default="")
-    #item_main_photo = models.ImageField(upload_to='img/main/', max_length=255, null=True, blank=True)
-    #tem_pictures_list = []
+
+    # item_main_photo = models.ImageField(upload_to='img/main/', max_length=255, null=True, blank=True)
+    # tem_pictures_list = []
     # TODO pip install Pillow for image field
+
+    item_main_photo = models.ImageField(upload_to='item_image', blank=True)
 
     def __str__(self):
         return self.item_name
+
+
+# class UserProfile(models.Model):
+#     user = models.OneToOneField(User)
+#
+#     def create_user(self):
+#         pass
+
+# class Basket(models.Model):
+#     user = models.OneToOneField(UserProfile)
+
 
--- knittingshop/urls.py
+++ knittingshop/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+from django.conf import  settings
+from django.conf.urls.static import static
 
 app_name = 'knittingshop'
 urlpatterns = [
@@ -8,4 +9,4 @@ urlpatterns = [
     path('<int:question_id>/', views.detail, name='detail'),
     path('gallery/', views.gallery, name='gallery'),
     path('test/', views.testindex, name='testindex')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
